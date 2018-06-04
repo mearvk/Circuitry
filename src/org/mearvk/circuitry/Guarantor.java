a41ed39991d1cc50aa35ced424e9d2e4acd188a7
Max Rupplin <mearvk@outlook.com>
2018-06-04 19:05:45 -0400

    Latest

--- src/org/mearvk/circuitry/Guarantor.java
+++ src/org/mearvk/circuitry/Guarantor.java
@@ -1,8 +1,31 @@
 package org.mearvk.circuitry;
 
+import org.mearvk.ancellaries.SystemComponent;
+
 /**
  * The Guarantor class will reference a XML document to guarantee a condition against a state or state change.  The purpose is to avoid race-condition in changing system components.
  */
 
-public class Guarantor {
+public class Guarantor extends SystemComponent {
+
+
+    @Override
+    public void latch() throws Exception {
+
+    }
+
+    @Override
+    public void unlatch() throws Exception {
+
+    }
+
+    @Override
+    public void ready() throws Exception {
+
+    }
+
+    @Override
+    public void init() throws Exception {
+
+    }
 }
