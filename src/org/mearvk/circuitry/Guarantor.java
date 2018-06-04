f6755d803240264ad713eced04f1db1d9dceeec8
Max Rupplin <mearvk@outlook.com>
2018-06-04 18:48:06 -0400

    Latest

---
+++ src/org/mearvk/circuitry/Guarantor.java
@@ -0,0 +1,8 @@
+package org.mearvk.circuitry;
+
+/**
+ * The Guarantor class will reference a XML document to guarantee a condition against a state or state change.  The purpose is to avoid race-condition in changing system components.
+ */
+
+public class Guarantor {
+}
